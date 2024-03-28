059fecef261bc226a40416d05bf883d5f91ddbfd
alvinadet <alvinadetyaworks@gmail.com>
2024-03-28 09:45:03 +0800

    feat: add pagination

---
+++ dto/pagination_dto.go
@@ -0,0 +1,48 @@
+package dto
+
+import (
+	"strings"
+)
+
+type Pagination struct {
+	Page       int         `json:"page" query:"page"`
+	Limit      int         `json:"limit" query:"limit"`
+	SortBy     string      `json:"-" query:"sortBy"`
+	SortOrder  string      `json:"-" query:"sortOrder"`
+	Keyword    string      `json:"-" query:"keyword"`
+	TotalData  int64       `json:"totalData"`
+	TotalPages int         `json:"total_pages"`
+	Rows       interface{} `json:"-"`
+}
+
+func (p *Pagination) GetOffset() int {
+	return (p.GetPage() - 1) * p.GetLimit()
+}
+
+func (p *Pagination) GetLimit() int {
+	if p.Limit == 0 {
+		p.Limit = 10
+	}
+	return p.Limit
+}
+
+func (p *Pagination) GetPage() int {
+	if p.Page == 0 {
+		p.Page = 1
+	}
+	return p.Page
+}
+
+func (p *Pagination) GetSort() string {
+	sortDir := strings.ToUpper(p.SortOrder)
+
+	if p.SortBy == "" && (sortDir == "") {
+		return ""
+	}
+
+	if sortDir == "" {
+		p.SortOrder = "ASC"
+	}
+
+	return p.SortBy + " " + sortDir
+}
--- repository/utils.go
+++ repository/utils.go
@@ -1,6 +1,13 @@
 package repository
 
-import "strings"
+import (
+	"math"
+	"strings"
+
+	"github.com/anoaland/xgo/dto"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
 
 func SetKeywordLikeVarsByTotalExpr(keyword string, total int) (vars []interface{}) {
 	for i := 0; i < total; i++ {
@@ -12,4 +19,44 @@ func SetKeywordLikeVarsByTotalExpr(keyword string, total int) (vars []interface{
 
 func LowerLikeQuery(field string) string {
 	return "LOWER(" + field + ") LIKE ?"
+}
+
+func FilterPaginate(DB *gorm.DB, modelName interface{}, pagination *dto.Pagination, clauses []clause.Expression, joins []string) func(db *gorm.DB) *gorm.DB {
+	var totalRows int64
+	tx := DB.Model(modelName).Clauses(clauses...)
+
+	if len(joins) > 0 {
+		tx = txWithJoins(tx, joins)
+	}
+
+	tx.Count(&totalRows)
+
+	pagination.TotalData = totalRows
+	totalPages := int(math.Ceil(float64(totalRows) / float64(pagination.GetLimit())))
+	pagination.TotalPages = totalPages
+
+	return func(db *gorm.DB) *gorm.DB {
+
+		if len(joins) > 0 {
+			db = txWithJoins(db, joins)
+		}
+
+		return db.Clauses(clauses...).Offset(pagination.GetOffset()).Limit(pagination.GetLimit()).Order(pagination.GetSort())
+	}
+}
+
+func txWithJoins(tx *gorm.DB, joinList []string) *gorm.DB {
+	for _, join := range joinList {
+		tx.Joins(join)
+	}
+
+	return tx
+}
+
+func txWithPeloads(tx *gorm.DB, preloadList []string) *gorm.DB {
+	for _, join := range preloadList {
+		tx.Preload(join)
+	}
+
+	return tx
 }
