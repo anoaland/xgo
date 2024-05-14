1a8a8b48b9002ff14a4a4afae8d0c419e6edd34b
Budi Adiono <budiadiono@gmail.com>
2024-05-14 14:55:22 +0700

    perf: ⚡️ better automigration

--- db/utils/print-auto-migrate-sql.go
+++ db/utils/print-auto-migrate-sql.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"strings"
 	"time"
@@ -18,8 +19,15 @@ type RecorderLogger struct {
 func (r *RecorderLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
 	sql, code := fc()
 
+	if strings.HasPrefix(sql, "SELECT") {
+		r.Interface.Trace(ctx, begin, fc, err)
+		return
+	}
+
 	if code == 0 {
-		if !strings.Contains(sql, "pg_catalog.pg_description") {
+		if !strings.HasPrefix(sql, "SELECT") && !strings.Contains(sql, "pg_catalog.pg_description") {
+			// r.Interface.Trace(ctx, begin, fc, err)
+			log.Println("==============================", sql)
 			r.Statements = append(r.Statements, sql)
 		}
 	}
@@ -30,12 +38,22 @@ func (r *RecorderLogger) Trace(ctx context.Context, begin time.Time, fc func() (
 // db: the database connection
 // dst: the model structs to auto migrate
 // string: the SQL statements for auto migrating the given model structs
-func PrintAutoMigrateSql(db *gorm.DB, dst ...interface{}) string {
-	// thanks to: https://stackoverflow.com/a/66246127/1586914
+func PrintAutoMigrateSqlx(db *gorm.DB, dst ...interface{}) string {
 
+	db.Config.PrepareStmt = true
+
+	// thanks to: https://stackoverflow.com/a/66246127/1586914
 	recorder := RecorderLogger{logger.Default.LogMode(logger.Silent), []string{}}
-	session := db.Session(&gorm.Session{DryRun: true, Logger: &recorder})
-	err := session.AutoMigrate(dst...)
+	session := db.Session(
+		&gorm.Session{
+			DryRun: true,
+			Logger: &recorder,
+		})
+	// err := session.AutoMigrate(dst...)
+
+	migrator := session.Dialector.Migrator(session)
+	err := migrator.AutoMigrate(dst...)
+
 	if err != nil {
 		log.Fatalf("failed to generate automigrate sql: %v", err)
 	}
@@ -43,4 +61,23 @@ func PrintAutoMigrateSql(db *gorm.DB, dst ...interface{}) string {
 	sql := strings.Join(recorder.Statements, ";\r\n")
 
 	return sql
+}
+
+// thanks to: https://github.com/go-gorm/gorm/issues/3851#issuecomment-929752108
+func PrintAutoMigrateSql(db *gorm.DB, dst ...interface{}) string {
+	tx := db.Begin()
+	var statements []string
+	tx.Callback().Raw().Register("record_migration", func(tx *gorm.DB) {
+		statements = append(statements, tx.Statement.SQL.String())
+	})
+	if err := tx.AutoMigrate(dst...); err != nil {
+		panic(err)
+	}
+	tx.Rollback()
+	tx.Callback().Raw().Remove("record_migration")
+	for _, s := range statements {
+		fmt.Println(s)
+	}
+
+	return strings.Join(statements, ";\r\n")
 }
